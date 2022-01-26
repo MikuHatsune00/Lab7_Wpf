beb94e47945e927f26ce4db95d5d5e991c79ea45
Kseniia <balerinagizelle@mail.ru>
2022-01-26 16:18:09 +0300

    added themes

--- Lab7_Wpf/MainWindow.xaml.cs
+++ Lab7_Wpf/MainWindow.xaml.cs
@@ -25,7 +25,26 @@ namespace Lab7_Wpf
         public MainWindow()
         {
             InitializeComponent();
+            List<string> styles = new List<string>() { "Светлая тема", "Темная тема" };
+            styleBox.ItemsSource = styles;
+            styleBox.SelectionChanged += ThemeChange;
+            styleBox.SelectedIndex = 0;
+            
         }
+
+        private void ThemeChange(object sender, SelectionChangedEventArgs e)
+        {
+            int styleIndex = styleBox.SelectedIndex;
+            Uri uri = new Uri("LightTheme.xaml", UriKind.Relative);
+            if (styleIndex==1)
+            { uri = new Uri("DarkTheme.xaml", UriKind.Relative); }
+
+            ResourceDictionary resource = Application.LoadComponent(uri) as ResourceDictionary;
+
+            Application.Current.Resources.MergedDictionaries.Add(resource);
+
+        }
+
         private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             string fontName = (sender as ComboBox).SelectedItem as string;
